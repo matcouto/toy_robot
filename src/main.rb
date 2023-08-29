eb8d92b701cb8d9a452cc02b9f0ad497c593e013
Mateus Couto <mateux@gmail.com>
2023-08-30 07:51:49 +1000

    Rescuing and Logging errors inside the ToyRobotGame class

--- src/main.rb
+++ src/main.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal: true
 
-require 'logger'
+# main.rb
+
 require './src/toy_robot_game'
 require './src/services/commands/processor_factory'
 require './src/domain/table'
 require './src/domain/robot'
+
+def main
+  display_welcome_message
+  logger = Logger.new($stdout)
+  toy_robot_game = setup_toy_robot_game(logger)
+
+  loop do
+    print '> '
+    command = gets.chomp.upcase
+    break if command == 'EXIT'
+
+    toy_robot_game.run(command)
+  rescue Interrupt
+    break
+  end
+end
 
 def display_welcome_message
   project_root = File.expand_path('..', 'toy_robot')
@@ -12,41 +29,11 @@ def display_welcome_message
   puts File.read(welcome_message_path)
 end
 
-def display_errors(game_errors)
-  return if game_errors.empty?
-
-  logger = Logger.new($stdout)
-  logger.level = Logger::ERROR
-  game_errors.each { |error| logger.error(error) }
-end
-
-def setup_game
+def setup_toy_robot_game(logger)
   table = Table.new(5, 5)
   robot = Robot.new
   processor_factory = Commands::ProcessorFactory.new(table, robot)
-  ToyRobotGame.new(table: table, robot: robot, processor_factory: processor_factory)
-end
-
-def main
-  display_welcome_message
-  game = setup_game
-
-  loop do
-    print '> '
-
-    command = gets.upcase.chomp
-    if command == 'EXIT'
-      # Displays errors before exiting via 'EXIT' command
-      display_errors(game.errors)
-      break
-    else
-      game.process(command.upcase)
-    end
-  rescue Interrupt
-    # Displays errors before exiting via ctrl+c
-    display_errors(game.errors)
-    break
-  end
+  ToyRobotGame.new(table: table, robot: robot, processor_factory: processor_factory, logger: logger)
 end
 
 main
